bcc295194520f0e55266d96170154a1ab7ab8c91
nicolas jolys <jolys.nicolas@gmail.com>
2023-09-08 15:36:45 +0200

    fix comment

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 // import { BellAlertIcon } from "@heroicons/react/24/outline"
 
-// import { navigation, reservationLink } from "@/lib/utils"
+import { navigation } from "@/lib/utils"
 
 import Burger from "./Burger"
 // import { buttonVariants } from "./ui/button"
