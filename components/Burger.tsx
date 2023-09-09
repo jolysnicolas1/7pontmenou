03f6e74838c5dc7952fc83ecf998d68c80b666e6
nicolas jolys <jolys.nicolas@gmail.com>
2023-09-09 17:59:40 +0200

    Fix

--- components/Burger.tsx
+++ components/Burger.tsx
@@ -6,7 +6,7 @@ import {
   // BellAlertIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline"
-import Link from "next/link"
+// import Link from "next/link"
 import { useState } from "react"
 
 import { navigation } from "@/lib/utils"
