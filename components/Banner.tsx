b4bb7169d0ceefd21dc8b9f4c80b1498ddbee710
nicolas jolys <jolys.nicolas@gmail.com>
2023-10-18 12:33:47 +0200

    Add picture old + Add breaking news + numbers

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -2,7 +2,7 @@
 export default function Banner() {
   return (
     <div className="relative isolate overflow-hidden bg-slate-950 w-screen">
-      <div className="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
+      <div className="px-6 py-16 sm:px-6 sm:py-24 lg:px-8">
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="text-3xl font-bold tracking-tight text-slate-50 sm:text-4xl">
             Appel à dons et prêts ✨
@@ -16,14 +16,14 @@ export default function Banner() {
           after:absolute
           after:-top-10
           after:right-0
-          after:content-['160.000_euros']
+          after:content-['211.000_euros']
           ">
             <div className="w-10/12 bg-gradient-to-r from-white to-myGreen h-8 relative 
               after:text-white
               after:absolute
               after:top-10
               after:right-0
-              after:content-['130.300_euros']
+              after:content-['164.430_euros']
               before:absolute
               before:bg-red-700
               before:h-4
@@ -33,7 +33,7 @@ export default function Banner() {
               before:-bottom-2
             "></div>
           </div>
-
+          <div className="text-white text-xl pt-12">Un grand merci à toutes et tous pour les encouragements et les 164.300 euros déjà récoltés 💙</div>
         </div>
       </div>
       <svg
