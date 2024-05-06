e55f0a8eeb7f1fd345451d0c84e858bd57146ad0
jolysnicolas1 <jolys.nicolas1@gmail.com>
2024-05-06 19:34:17 +0200

    Change subtitle

--- components/HeroContent.tsx
+++ components/HeroContent.tsx
@@ -16,10 +16,12 @@ export default function HeroContent() {
         <div className="mx-auto max-w-full pt-32 sm:pt-48 lg:pt-56 pb-12">
           <div className="text-center flex flex-col justify-center items-center space-y-12 text-white">
             <h1 className="uppercase font-pitch font-bold tracking-[0.085em] text-2xl sm:text-4xl md:text-5xl">
-              Projet d&apos;Achat du 7 Pont-Menou
+              7 Pont-Menou
             </h1>
             <p className="mt-4 sm:text-lg leading-7">
-              Le 7pontmenou est un immeuble dans la célèbre localité de Pont Menou, terre d&apos;aventures aux confins des Côtes-d&apos;Armor et du Finistère, port d&apos;attache et de retour d&apos;un certain nombre de personnes * en Dérive * ces dernières années.
+                Le 7pontmenou est un immeuble dans la célèbre localité de Pont Menou, terre d'aventures aux confins des Côtes-d'Armor et du Finistère, port d'attache et de retour d'un certain nombre de personnes * en Dérive * ces dernières années.
+
+                Le 7 Pont menou c’est aussi une asso qui achète cet immeuble et souhaite y modeler de l’habitat, du « prendre soin » et de l’accueil.
             </p>
           </div>
         </div>
